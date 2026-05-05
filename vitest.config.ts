0a4771c0f877021390758467743345aa2624b2a4
frederico-rocha <f.frederico@gmail.com>
2026-05-05 10:07:59 +0200

    removed more fields to make it work in non git hub

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/desassossego",
+  //base: "/desassossego",
   test: {
     environment: "jsdom",
     globals: true,
