f11213871d2ce671efcb17437cf397f7a0f4ba98
frederico-rocha <f.frederico@gmail.com>
2026-05-05 10:06:52 +0200

    remove browserrouter for non github

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/desassossego">
+      {/* <BrowserRouter basename="/desassossego"> */}
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
