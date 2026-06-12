b318e746624dabd95da8ae5becb58123e0cd17d2
frederico-rocha <f.frederico@gmail.com>
2026-06-12 10:58:00 +0200

    router updated to run in github pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,10 @@ const App = () => (
       <Toaster />
       <Sonner />
       {/* <BrowserRouter basename="/desassossego"> */}
-      <BrowserRouter>
+      <BrowserRouter basename="/desassossego">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/equipa/:slug" element={<TeamMember />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
